dc2a439cec13c80cb7237bc3b0818631198a3232
Sanshiroh SAKAI <sansyo@gmail.com>
2024-12-24 16:45:26 +0900

    Update: ユーザー削除機能を追加

--- stores/userStore.ts
+++ stores/userStore.ts
@@ -3,8 +3,12 @@ import { createJSONStorage, devtools, persist } from "zustand/middleware";
 import type { User } from "@/types/schema";
 
 type UserStore = {
+  /** ユーザー一覧 */
   users: User[];
+  /** ユーザーを追加 */
   addUser: (user: User) => void;
+  /** ユーザーを削除 */
+  deleteUser: (id: string) => void;
 };
 
 export const useUserStore = create<UserStore>()(
@@ -14,9 +18,19 @@ export const useUserStore = create<UserStore>()(
         users: [],
         addUser: (user) =>
           set(
-            (state) => ({ users: [user, ...state.users] }),
+            (state) => ({
+              users: [user, ...state.users],
+            }),
             false,
             "User/addUser",
+          ),
+        deleteUser: (id) =>
+          set(
+            (state) => ({
+              users: state.users.filter((user) => user.id !== id),
+            }),
+            false,
+            "User/deleteUser",
           ),
       }),
       {
