9f70ebcdf3d485bb5dca930fb634b1574f73b2da
Sanshiroh SAKAI <sansyo@gmail.com>
2024-12-26 09:11:40 +0900

    Update: ユーザーリスト幅調整

--- components/features/user-list/UserList.tsx
+++ components/features/user-list/UserList.tsx
@@ -63,13 +63,17 @@ export const UserList = () => {
           <TableHeader>
             <TableRow>
               <TableHead className="w-px">&nbsp;</TableHead>
-              <TableHead className="whitespace-nowrap">表示名</TableHead>
-              <TableHead className="whitespace-nowrap">名前</TableHead>
+              <TableHead className="w-56 whitespace-nowrap">表示名</TableHead>
+              <TableHead className="w-72 whitespace-nowrap">名前</TableHead>
               <TableHead className="whitespace-nowrap">
                 メールアドレス
               </TableHead>
-              <TableHead className="whitespace-nowrap">ステータス</TableHead>
-              <TableHead className="whitespace-nowrap">アクセス権</TableHead>
+              <TableHead className="w-40 whitespace-nowrap">
+                ステータス
+              </TableHead>
+              <TableHead className="w-40 whitespace-nowrap">
+                アクセス権
+              </TableHead>
               <TableHead className="whitespace-nowrap">&nbsp;</TableHead>
             </TableRow>
           </TableHeader>
